16c2cf3a86daccfa980874a02d4879303b1b043e
T-A-U <153134801+T-A-U@users.noreply.github.com>
2024-02-25 18:10:11 -0700

    Objective 1 met but no requirements yet.

--- server.js
+++ server.js
@@ -1,14 +1,47 @@
 
 //express server template
-const express = require ('express')
+const express = require ('express');
 
-const app = express()
-let PORT = 3000 
+const app = express();
+let PORT = 3000; 
 
 app.get('/', (req , res)=>{
     res.send('Hello, World!')
-})
+});
 
-app.listen(PORT, ()=>{
+
+app.get("/pokemon", (req,res) => {
+    const pokemon = [
+        {
+            id: 1,
+            name: "Bulbasauar",
+        },
+        {
+            id: 2,
+            name: "Ivysaur",
+        },
+        {
+        id: 3,
+        name: "Venusaur",
+        },
+    ];
+  
+    res.json(pokemon);
+});
+
+
+
+
+
+
+
+
+
+
+//Must be the last thing on the list
+app.listen(PORT, (error)=>{
+    if(!error)
     console.log('server is listening on : ${PORT}')
-})
+    else
+    console.log("Error occured server cannot start", error);
+});
