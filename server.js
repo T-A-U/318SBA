1bb00c4a5584fdfacb841de78d862553f8d9347d
T-A-U <153134801+T-A-U@users.noreply.github.com>
2024-02-27 21:31:51 -0700

    Include query parameters for data filtering.

--- server.js
+++ server.js
@@ -73,7 +73,7 @@ app.get("/HPrefill", (req,res) => {
 });
 
 
-// Create POST routes for data, as appropriate. At least one data category should allow for client creation via a POST request. Dunno what theme this project even is yet. So ill have to come back to this.
+// Create POST routes for data, as appropriate. At least one data category should allow for client creation via a POST request. Dunno what theme this project even is yet. So ill have to come back to this. Somethin' pokemon themed
 app.post('/users/new', async (req, res) => {
     try {
      const user = await User.add({ userFullName: req.user.name, userName: req.user.username, userEmail: req.user.email });
@@ -84,8 +84,8 @@ app.post('/users/new', async (req, res) => {
     }
    });
 
-// Create PATCH or PUT routes for data, as appropriate. At least one data category should allow for client manipulation via a PATCH or PUT request. Still dunno theme yet so must come back to this.
-   app.put('/user/update', async (req, res) => {
+// Create PATCH or PUT routes for data, as appropriate. At least one data category should allow for client manipulation via a PATCH or PUT request. Still dunno theme yet so must come back to this. Somethin pokemon themed
+app.put('/user/update', async (req, res) => {
     try {
      const user = await User.update({ userFullName: req.user.name, userName: req.user.username, userEmail: req.user.email });
      res.json(user);
@@ -94,6 +94,49 @@ app.post('/users/new', async (req, res) => {
      res.json({ error: err.toString() });
     }
     })
+
+// Create DELETE routes for data, as appropriate. At least one data category should allow for client deletion via a DELETE request. come back to. Somethin' pokemon themed
+    app.delete('/user/delete', async (req, res) => {
+        try {
+         const user = await User.delete({ userFullName: req.user.name, userName: req.user.username, userEmail: req.user.email });
+         res.json(user);
+        } catch (err) {
+         console.log(err);
+         res.json({ error: err.toString() });
+        }
+       });
+
+
+
+// Include query parameters for data filtering, where appropriate. At least one data category should allow for additional filtering through the use of query parameters      
+
+app.get("/pokeballs?cost=200", (req,res) => {
+    const pokeballs = [
+        {
+            id: 1,
+            name: "pokeball",
+            cost:200
+        },
+        {
+            id: 2,
+            name: "Greatball",
+            cost:600
+        },
+        {
+        id: 3,
+        name: "Ultraball",
+        cost:1200
+        },
+    ];
+  
+    res.json(pokeballs.cost=200);
+});
+
+
+
+
+
+
 
 
 
