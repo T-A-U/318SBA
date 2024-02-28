a227ec62ab09d36955d2a22abb73b4a23006ec0a
T-A-U <153134801+T-A-U@users.noreply.github.com>
2024-02-27 20:35:23 -0700

    Use at least 3 diff data categories

--- server.js
+++ server.js
@@ -5,11 +5,14 @@ const express = require ('express');
 const app = express();
 let PORT = 3000; 
 
+
+//GET routes  for data exposed t0 client
 app.get('/', (req , res)=>{
     res.send('Hello, World!')
 });
 
-
+//3 diff data categories
+//1
 app.get("/pokemon", (req,res) => {
     const pokemon = [
         {
@@ -29,6 +32,45 @@ app.get("/pokemon", (req,res) => {
     res.json(pokemon);
 });
 
+//2
+app.get("/pokeballs", (req,res) => {
+    const pokeballs = [
+        {
+            id: 1,
+            name: "pokeball",
+        },
+        {
+            id: 2,
+            name: "Greatball",
+        },
+        {
+        id: 3,
+        name: "Ultraball",
+        },
+    ];
+  
+    res.json(pokeballs);
+});
+
+//3
+app.get("/HPrefill", (req,res) => {
+    const HPrefill = [
+        {
+            id: 1,
+            name: "potion",
+        },
+        {
+            id: 2,
+            name: "superpotion",
+        },
+        {
+        id: 3,
+        name: "hyperpotion",
+        },
+    ];
+  
+    res.json(HPrefill);
+});
 
 
 
@@ -36,12 +78,11 @@ app.get("/pokemon", (req,res) => {
 
 
 
-
-
+//error handling?
 //Must be the last thing on the list
 app.listen(PORT, (error)=>{
     if(!error)
-    console.log('server is listening on : ${PORT}')
+    console.log(`server is listening on : ${PORT}`)
     else
     console.log("Error occured server cannot start", error);
 });
